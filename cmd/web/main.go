1d80fe9efc73909bb8c92214bd7cd556a301cb48
Li Liang <liliang.imut@gmail.com>
2021-10-24 14:06:21 +0800

    feat: flag

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
@@ -14,9 +19,9 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting server on :4000")
+	log.Printf("Starting server on: %s\n", *addr)
 
-	err := http.ListenAndServe(":4000", mux)
+	err := http.ListenAndServe(*addr, mux)
 
 	log.Fatal(err)
 }
