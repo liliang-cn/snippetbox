e57e5a8750f388c0f0076ca2825624f2f3f0d9cf
Li Liang <liliang.imut@gmail.com>
2021-10-26 17:35:22 +0800

    feat: render items

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -21,29 +21,29 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	for _, snippet := range s {
-		fmt.Fprintf(w, "%v\n", snippet)
+	data := &templateData{
+		Snippets: s,
 	}
 
-	//files := []string{
-	//	"./ui/html/home.page.tmpl",
-	//	"./ui/html/base.layout.tmpl",
-	//	"./ui/html/footer.partial.tmpl",
-	//}
-	//
-	//ts, err := template.ParseFiles(files...)
-	//
-	//if err != nil {
-	//	app.errorLog.Println(err.Error())
-	//	app.notFound(w)
-	//}
-	//
-	//err = ts.Execute(w, nil)
-	//if err != nil {
-	//	app.errorLog.Println(err.Error())
-	//	app.serverError(w, err)
-	//	return
-	//}
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+
+	if err != nil {
+		app.errorLog.Println(err.Error())
+		app.notFound(w)
+	}
+
+	err = ts.Execute(w, data)
+	if err != nil {
+		app.errorLog.Println(err.Error())
+		app.serverError(w, err)
+		return
+	}
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
