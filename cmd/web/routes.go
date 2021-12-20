f6e9d3dfbae61cf8c1f6cb7bca037d6ebc4047f1
Li Liang <liliang.imut@gmail.com>
2021-12-20 18:27:48 +0800

    feat: create snippet

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,14 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"github.com/bmizerany/pat"
+	"github.com/justinas/alice"
+	"net/http"
+)
 
 func (app *application) routes() http.Handler {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
+	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+	//mux := http.NewServeMux()
+	mux := pat.New()
+	mux.Get("/", http.HandlerFunc(app.home))
+	mux.Get("/snippet", http.HandlerFunc(app.showSnippet))
+	mux.Get("/snippet/create", http.HandlerFunc(app.createSnippetForm))
+	mux.Post("/snippet/create", http.HandlerFunc(app.createSnippet))
+	mux.Get("/snippet/:id", http.HandlerFunc(app.showSnippet)) // Moved down
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+	mux.Get("/static/", http.StripPrefix("/static", fileServer))
+	return standardMiddleware.Then(mux)
 }
